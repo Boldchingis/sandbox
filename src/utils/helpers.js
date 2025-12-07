35221d975451da7f3650651ce7eb83ed507d16bd
Boldchingis <bboldchingis@gmail.com>
2025-12-07 23:38:56 +0000

    fix: fix race condition

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,3 +2,4 @@
 // 2025-12-07 04:48:22 +08 - chore: improve error messages
 // 2025-12-07 10:19:11 +08 - fix: fix security vulnerability
 // 2025-12-08 02:53:15 +08 - fix: fix race condition
+// 2025-12-08 07:38:56 +08 - fix: fix race condition
